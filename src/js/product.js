13b4c8b96e8dc17233d63669d07ed6fafa0deb92
favym93 <favym93@example.com>
2026-07-03 14:31:17 -0500

    Fix cart overwriting and add function comment

--- src/js/product.js
+++ src/js/product.js
@@ -3,6 +3,7 @@ import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
+// Retrieves the existing cart array, adds the new product, and updates local storage
 function addProductToCart(product) {
   const currentCart = getLocalStorage('so-cart') || [];
 
