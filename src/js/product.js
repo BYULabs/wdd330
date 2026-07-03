c2bebc39ea2c476843b061f85b37a355dec7a083
nephiasha-eassist <nephi.asha@eassist.me>
2026-07-03 18:32:43 +0100

    fix: Add item to cart broken

--- src/js/product.js
+++ src/js/product.js
@@ -3,13 +3,20 @@ import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+function addProductToCart(cart) {
+  setLocalStorage('so-cart', cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
+  // This creates a cart array in local storage if it doesn't exist, or retrieves the existing cart array if it does exist.
+  let cart = JSON.parse(localStorage.getItem('so-cart')) || [];
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+
+  // adds the product to the cart array
+  cart.push(product);
+
+  //saves it to the local storage
+  addProductToCart(cart);
 }
 
 // add listener to Add to Cart button
