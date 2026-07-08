a2334126e94059476c0b80c94a4522c2761fb3d1
Sebastian Iturralde <itusebastian@gmail.com>
2026-07-08 10:52:16 -0500

    Refactor product.js to streamline imports and initialize ProductDetails

--- src/js/product.js
+++ src/js/product.js
@@ -1,36 +1,9 @@
-import { setLocalStorage, getLocalStorage, getParam } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
-const dataSource = new ProductData('tents');
 const productId = getParam('product');
+const dataSource = new ProductData('tents');
 
-// --- TEST block ---
-async function testProductFetch() {
-  console.log("Extracted Product ID from URL:", productId);
-  
-  if (productId) {
-    const product = await dataSource.findProductById(productId);
-    console.log("Fetched Product Data:", product);
-  } else {
-    console.warn("No product ID found in the URL query string! Did you click a link with '?product=xxxx'?");
-  }
-}
-testProductFetch();
-// ------------------
-
-// Retrieves the existing cart array, checks for duplicates, and updates local storage
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  setLocalStorage("so-cart", cartItems);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
