5901c29f53a661e0bf9f24ac42e4897200bae881
Sebastian Iturralde <itusebastian@gmail.com>
2026-07-10 09:04:29 -0500

    feat(utils): add renderListWithTemplate function for dynamic DOM rendering

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -32,3 +32,30 @@ export function getParam(param) {
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
 }
+
+/**
+ * Renders a list of items into the DOM using a template function.
+ * @param {Function} templateFn - The function that returns an HTML string for an item.
+ * @param {HTMLElement} parentElement - The DOM element where the list will be inserted.
+ * @param {Array} list - The array of data items to render.
+ * @param {string} [position="afterbegin"] - Where to insert the HTML relative to the parentElement.
+ * @param {boolean} [clear=false] - Whether to clear the parent element's content first.
+ */
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = 'afterbegin',
+  clear = false,
+) {
+  // If clear is true, wipe out the existing HTML first
+  if (clear) {
+    parentElement.innerHTML = '';
+  }
+
+  // Map each item to its template HTML string and join them
+  const htmlStrings = list.map(templateFn);
+
+  // Insert the joined HTML string into the DOM
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
+}
